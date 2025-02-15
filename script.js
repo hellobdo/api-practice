0de7776a7a9d1291c61b4fbafd1c941ec9766c70
Bruno de Oliveira <badeoliveirag@gmail.com>
2025-02-15 19:04:44 -0300

    added button to refresh pic

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const img = document.getElementById("image");
+const updatePic = document.getElementById("updatePic");
 
-fetch(
-    'https://api.giphy.com/v1/gifs/translate?api_key="API_KEY"=cats',
-    {mode: 'cors'}
-    )
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src=response.data.images.original.url
-});
+function apiFetch () {
+    fetch(
+        'https://api.giphy.com/v1/gifs/translate?api_key="APIKEY"&s=cats',
+        { mode: 'cors' }
+        )
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            img.src=response.data.images.original.url
+    });
+}
+
+updatePic.addEventListener("click", handleClick);
+
+function handleClick () {
+    img.src="";
+    apiFetch();
+    return;
+}
+
+apiFetch();
