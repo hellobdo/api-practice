fe3ad9e3554d9eefb56d5fd698b472fc8b74823e
Bruno de Oliveira <badeoliveirag@gmail.com>
2025-02-15 19:39:03 -0300

    added button for search new gifs

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const img = document.getElementById("image");
 const updatePic = document.getElementById("updatePic");
+const searchBar = document.getElementById("searchBar");
+const submit = document.getElementById("submit");
+let api_key = ""
+let blankUrl = 'https://api.giphy.com/v1/gifs/translate?api_key='
+let baseUrl = blankUrl + api_key + "&s="
 
-function apiFetch () {
+function apiFetch (url) {
     fetch(
-        'https://api.giphy.com/v1/gifs/translate?api_key="APIKEY"&s=cats',
+        url,
         { mode: 'cors' }
         )
         .then(function(response) {
@@ -15,11 +20,18 @@ function apiFetch () {
 }
 
 updatePic.addEventListener("click", handleClick);
+submit.addEventListener("click", handleClick);
 
-function handleClick () {
+function handleClick (event) {
     img.src="";
-    apiFetch();
-    return;
+    let newUrl = baseUrl;
+    if (event.target.id === "submit") {
+        console.log(searchBar.value);
+        let newSearch = searchBar.value.trim();
+        newUrl += newSearch;
+        console.log(newUrl);
+    }
+    apiFetch(newUrl);
 }
 
-apiFetch();
+apiFetch(baseUrl + "cats");
